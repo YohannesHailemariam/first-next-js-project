a4094bd3651fb1501b96f96e49a4de88f0f79046
iamjohnhm <jhm90822@gmail.com>
2024-05-07 10:04:13 +0300

    Added a feature when a user gives a name in the input text field, it will return the predicted age, gender and country. This was enabled by fetching data for the age, gender and country from three different apis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function Page(){
+  const [inputVal, setInputVal] = useState("");
+  const {push} = useRouter()
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    push(`/prediction/${inputVal}`)
+  }
+
+  return (  
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <div className="p-4 shadow-md bg-white rounded-md">
+              <h1 className="text-2xl font-semibold mb-4 text-black">Enter Your Name</h1>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-3">
+              <input 
+              type="text" 
+              className="w-full p-2 border border-gray-300 rounded text-black"
+              placeholder="Type your name..." 
+              value={inputVal}
+              onChange={(e) => setInputVal(e.target.value)} />
+              
+              <button 
+              type="submit"
+              className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+              >Predict Data</button>
+          </form>
+      </div>
   );
 }
